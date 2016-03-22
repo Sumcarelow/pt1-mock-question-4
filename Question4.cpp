5d5031a5f4bf8d760b253702921b7a3fa058cec8
samkelo <sumcarelow@gmail.com>
2016-03-22 22:47:33 +0200

    created base class and concrete classes

--- Question4.cpp
+++ Question4.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name: Samkelo Zondi
+// Student No: 214538321
+// Date: 22-March-2016
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // ***** QUESTION 4 ****
@@ -11,3 +11,34 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Animal{
+protected:
+	string newName;
+	string newDoes;
+
+
+public:
+	Animal();
+	void setValues(string a, string b){ newName = a; newDoes = b; }
+	virtual string name() = 0;
+	virtual string does() = 0;
+};
+
+class Bird :public Animal {
+public:
+	string name(){ return newName; };
+	string does(){ return newDoes; };
+
+};
+
+class Dog : public Animal{
+public:
+	string name(){ return newName; };
+	string does(){ return newDoes; };
+};
+
